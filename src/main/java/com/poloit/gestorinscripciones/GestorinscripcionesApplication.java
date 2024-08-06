7971feb7c3891304f5039ccd138b8e6b5fde602f
Ezequiel Bofill <ezequielb@epidataconsulting.com>
2024-08-06 10:33:40 -0300

    Commit Inicial

---
+++ src/main/java/com/poloit/gestorinscripciones/GestorinscripcionesApplication.java
@@ -0,0 +1,13 @@
+package com.poloit.gestorinscripciones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestorinscripcionesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestorinscripcionesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/poloit/gestorinscripciones/GestorinscripcionesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poloit.gestorinscripciones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestorinscripcionesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
