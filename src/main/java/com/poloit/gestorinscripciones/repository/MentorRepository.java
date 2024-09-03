e690e94c47a2d1eb389e29d2e4c9b71e3ef0d256
Bianca Stork <biancastrk@gmail.com>
2024-09-03 11:37:00 -0300

    update Mentor

--- src/main/java/com/poloit/gestorinscripciones/repository/MentorRepository.java
+++
@@ -1,9 +0,0 @@
-package com.poloit.gestorinscripciones.repository;
-
-import com.poloit.gestorinscripciones.model.Mentor;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MentorRepository extends JpaRepository<Mentor, Long> {
-}
