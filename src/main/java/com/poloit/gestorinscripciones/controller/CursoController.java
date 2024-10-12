6907b1864a8535d19bd7bc0eeec01451ab22a449
Bianca Stork <biancastrk@gmail.com>
2024-10-12 03:45:36 -0300

    endpoints para cursos/proyectos e inscripciones a proyectos activos

--- src/main/java/com/poloit/gestorinscripciones/controller/CursoController.java
+++ src/main/java/com/poloit/gestorinscripciones/controller/CursoController.java
@@ -1,9 +1,13 @@
 package com.poloit.gestorinscripciones.controller;
 
+import com.poloit.gestorinscripciones.exceptions.ResourceNotFoundException;
 import com.poloit.gestorinscripciones.model.Curso;
+import com.poloit.gestorinscripciones.model.User;
 import com.poloit.gestorinscripciones.service.CursoService;
+import com.poloit.gestorinscripciones.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.http.ResponseEntity;
 
 import java.util.List;
 import java.util.Optional;
@@ -13,15 +17,37 @@ import java.util.Optional;
 public class CursoController {
     @Autowired
     CursoService cursoService;
+    @Autowired
+    UserService userService;
 
-    @PostMapping()
-    public void crearCurso(@RequestBody Curso curso){
-        cursoService.crearCurso(curso);
-
+    @PostMapping
+    public ResponseEntity<Curso> crearCurso(@RequestBody Curso curso) {
+        Curso nuevoCurso = cursoService.crearCurso(curso);
+        return ResponseEntity.ok(nuevoCurso);
     }
+    @PostMapping("/{cursoId}/inscripcion")
+    public ResponseEntity<User> inscribirUsuario(@PathVariable Long cursoId, @RequestBody Long usuarioId) {
+        Curso curso = cursoService.cursoId(cursoId)
+                .orElseThrow(() -> new RuntimeException("Curso no encontrado"));
+
+        User usuario = userService.userId(usuarioId)
+                .orElseThrow(() -> new RuntimeException("Usuario no encontrado"));
+
+        usuario.setCurso(curso);
+        userService.actualizarUser(usuarioId, usuario);
+
+        return ResponseEntity.ok(usuario);
+    }
+
     @GetMapping()
     public List<Curso> mostrarTodos(){
         return cursoService.mostrarTodos();
+    }
+
+    @GetMapping("/activos")
+    public ResponseEntity<List<Curso>> obtenerCursosActivos() {
+        List<Curso> cursosActivos = cursoService.obtenerCursosActivos(); // Método que debes implementar
+        return ResponseEntity.ok(cursosActivos);
     }
 
     @GetMapping("/{id}")
